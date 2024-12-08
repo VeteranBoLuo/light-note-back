4a08bd3ac420a1be45162a9b13df0749c2dd8129
VeteranBoLuo <1902013368@qq.com>
2024-12-08 22:01:04 +0800

    日志查询去掉超级管理员

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   connectionLimit: 10, // 例如限制为10个连接
-  host: "139.9.83.16",
+  host: "127.0.0.1",
   port: 3306,
   user: "boluo",
   password: "123456",
