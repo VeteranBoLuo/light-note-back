a1dbd40899fe4c2d9c6015aa25887bd28039bdd0
w30037884 <wenjunqiu@huawei.com>
2025-03-29 18:14:51 +0800

    记录非法请求

--- app.js
+++ app.js
@@ -6,15 +6,40 @@ const bookmarkRouter = require('./router/bookmark');
 const opinionRouter = require('./router/opinion');
 const bodyParser = require('body-parser');
 const { logFunction } = require('./util/log');
-const { requestTime } = require('./util/common');
+const { requestTime, getClientIp } = require('./util/common');
+const pool = require('./db/index');
 
 // 建立一个Express服务器
 const app = express();
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 //  解析请求体中的JSON数据
 app.use(express.json());
+
 //  记录请求时间
 app.use(requestTime);
+app.use((req, res, next) => {
+  const { method, path, body, headers, query } = req;
+  const allowedOrigins = ['http://localhost:5173', 'https://boluo66.top', 'http://boluo66.top'];
+  const origin = req.headers.origin || req.headers.referer;
+  if (!allowedOrigins.some((url) => origin?.startsWith(url))) {
+    // 记录攻击事件
+    const log = {
+      attack_type: '非法请求来源',
+      request_method: method,
+      request_path: path,
+      source_ip: getClientIp(req),
+      payload: JSON.stringify({ ...body, ...query }),
+      user_agent: headers['user-agent'],
+      created_at: req.requestTime,
+    };
+    // 将攻击日志保存到数据库
+    pool.query('INSERT INTO attack_logs SET ?', [log]).catch((err) => {
+      console.error('攻击日志更新错误: ' + err.message);
+    });
+    return res.status(403).json({ code: 403, msg: '非法请求来源' });
+  }
+  next();
+});
 // 日志记录中间件
 app.use(logFunction);
 
