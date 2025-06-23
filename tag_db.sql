9c32d20579d4ea6ab8b93cc1c5af6b6b1c5fc387
w30037884 <wenjunqiu@huawei.com>
2025-06-23 09:24:25 +0800

    sql结构上传

--- tag_db.sql
+++ tag_db.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : cloud
+ Source Server         : local
  Source Server Type    : MySQL
  Source Server Version : 50743
- Source Host           : 139.9.83.16:3306
+ Source Host           : localhost:3306
  Source Schema         : tag_db
 
  Target Server Type    : MySQL
  Target Server Version : 50743
  File Encoding         : 65001
 
- Date: 07/12/2024 23:52:43
+ Date: 23/06/2025 09:22:37
 */
 
 SET NAMES utf8mb4;
@@ -24,17 +24,35 @@ DROP TABLE IF EXISTS `api_logs`;
 CREATE TABLE `api_logs`  (
   `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户ID',
-  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `request_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户调用接口的时间',
   `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '调用的接口路径',
-  `res` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `req` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `os` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统',
-  `browser` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '浏览器',
   `method` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求方法（如GET, POST等）',
+  `req` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'ip地址',
+  `system` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统信息',
+  `location` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `request_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户调用接口的时间',
   `del_flag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for attack_logs
+-- ----------------------------
+DROP TABLE IF EXISTS `attack_logs`;
+CREATE TABLE `attack_logs`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `attack_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '攻击类型（如SQL注入/XSS/暴力破解等）',
+  `request_method` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '请求方法（GET/POST等）',
+  `request_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '请求路径',
+  `source_ip` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '来源IP地址（支持IPv6）',
+  `payload` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '攻击载荷（如恶意参数、SQL语句等）',
+  `user_agent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '攻击者User-Agent',
+  `created_at` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '攻击时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_source_ip`(`source_ip`) USING BTREE,
+  INDEX `idx_created_at`(`created_at`) USING BTREE,
+  INDEX `idx_attack_type`(`attack_type`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 894 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '攻击日志表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for bookmark
@@ -53,6 +71,51 @@ CREATE TABLE `bookmark`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Table structure for files
+-- ----------------------------
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '上传用户ID',
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '上传时间',
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文件原名',
+  `file_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文件类型（MIME类型）',
+  `file_size` bigint(20) NOT NULL COMMENT '文件大小（字节）',
+  `file_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文件存储路径',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文件访问URL',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '文件信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for note
+-- ----------------------------
+DROP TABLE IF EXISTS `note`;
+CREATE TABLE `note`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `tags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `update_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `del_flag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for note_images
+-- ----------------------------
+DROP TABLE IF EXISTS `note_images`;
+CREATE TABLE `note_images`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `note_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `note_id`(`note_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
 -- Table structure for operation_logs
 -- ----------------------------
 DROP TABLE IF EXISTS `operation_logs`;
@@ -62,11 +125,25 @@ CREATE TABLE `operation_logs`  (
   `operation` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人员',
   `create_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建时间',
-  `os` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统',
-  `browser` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '浏览器',
   `del_flag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for opinion
+-- ----------------------------
+DROP TABLE IF EXISTS `opinion`;
+CREATE TABLE `opinion`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `img_array` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `del_flag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tag
@@ -78,8 +155,22 @@ CREATE TABLE `tag`  (
   `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `icon_url` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '图标地址',
   `create_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sort` int(10) NOT NULL DEFAULT 0,
   `del_flag` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for tag_bookmark_relations
+-- ----------------------------
+DROP TABLE IF EXISTS `tag_bookmark_relations`;
+CREATE TABLE `tag_bookmark_relations`  (
+  `tag_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `bookmark_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`tag_id`, `bookmark_id`) USING BTREE,
+  INDEX `fk_bookmark_id`(`bookmark_id`) USING BTREE,
+  CONSTRAINT `fk_bookmark_id` FOREIGN KEY (`bookmark_id`) REFERENCES `bookmark` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -88,24 +179,11 @@ CREATE TABLE `tag`  (
 DROP TABLE IF EXISTS `tag_relations`;
 CREATE TABLE `tag_relations`  (
   `tag_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `associated_tag_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`tag_id`, `associated_tag_id`) USING BTREE,
-  INDEX `fk_tag_associations_associated_tag`(`associated_tag_id`) USING BTREE,
-  CONSTRAINT `fk_tag_associations_associated_tag` FOREIGN KEY (`associated_tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_tag_associations_tag` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for tag_bookmark_relation
--- ----------------------------
-DROP TABLE IF EXISTS `tag_bookmark_relation`;
-CREATE TABLE `tag_bookmark_relation`  (
-  `tag_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `bookmark_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`tag_id`, `bookmark_id`) USING BTREE,
-  INDEX `fk_bookmark_id`(`bookmark_id`) USING BTREE,
-  CONSTRAINT `fk_bookmark_id` FOREIGN KEY (`bookmark_id`) REFERENCES `bookmark` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+  `related_tag_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`tag_id`, `related_tag_id`) USING BTREE,
+  INDEX `fk_tag_relations_related_tag`(`related_tag_id`) USING BTREE,
+  CONSTRAINT `fk_tag_relations_related_tag_id` FOREIGN KEY (`related_tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `fk_tag_relations_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -123,16 +201,30 @@ CREATE TABLE `user`  (
   `theme` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '主题',
   `head_picture` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '头像',
   `del_flag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
+  `create_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '注册时间',
+  `location` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Triggers structure for table api_logs
 -- ----------------------------
-DROP TRIGGER IF EXISTS `api_logs_id_before`;
+DROP TRIGGER IF EXISTS `a1`;
 delimiter ;;
-CREATE TRIGGER `api_logs_id_before` BEFORE INSERT ON `api_logs` FOR EACH ROW BEGIN
-     SET new.id = UUID();
+CREATE TRIGGER `a1` BEFORE INSERT ON `api_logs` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table attack_logs
+-- ----------------------------
+DROP TRIGGER IF EXISTS `a2`;
+delimiter ;;
+CREATE TRIGGER `a2` BEFORE INSERT ON `attack_logs` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
 END
 ;;
 delimiter ;
@@ -140,10 +232,32 @@ delimiter ;
 -- ----------------------------
 -- Triggers structure for table bookmark
 -- ----------------------------
-DROP TRIGGER IF EXISTS `bookmark_id_before`;
+DROP TRIGGER IF EXISTS `b1`;
 delimiter ;;
-CREATE TRIGGER `bookmark_id_before` BEFORE INSERT ON `bookmark` FOR EACH ROW BEGIN
-     SET new.id = UUID();
+CREATE TRIGGER `b1` BEFORE INSERT ON `bookmark` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table note
+-- ----------------------------
+DROP TRIGGER IF EXISTS `n1`;
+delimiter ;;
+CREATE TRIGGER `n1` BEFORE INSERT ON `note` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table note_images
+-- ----------------------------
+DROP TRIGGER IF EXISTS `n2`;
+delimiter ;;
+CREATE TRIGGER `n2` BEFORE INSERT ON `note_images` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
 END
 ;;
 delimiter ;
@@ -151,10 +265,21 @@ delimiter ;
 -- ----------------------------
 -- Triggers structure for table operation_logs
 -- ----------------------------
-DROP TRIGGER IF EXISTS `ope_id_before`;
+DROP TRIGGER IF EXISTS `o1`;
 delimiter ;;
-CREATE TRIGGER `ope_id_before` BEFORE INSERT ON `operation_logs` FOR EACH ROW BEGIN
-     SET new.id = UUID();
+CREATE TRIGGER `o1` BEFORE INSERT ON `operation_logs` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table opinion
+-- ----------------------------
+DROP TRIGGER IF EXISTS `o2`;
+delimiter ;;
+CREATE TRIGGER `o2` BEFORE INSERT ON `opinion` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
 END
 ;;
 delimiter ;
@@ -162,10 +287,10 @@ delimiter ;
 -- ----------------------------
 -- Triggers structure for table tag
 -- ----------------------------
-DROP TRIGGER IF EXISTS `tag_id_before`;
+DROP TRIGGER IF EXISTS `t1`;
 delimiter ;;
-CREATE TRIGGER `tag_id_before` BEFORE INSERT ON `tag` FOR EACH ROW BEGIN
-     SET new.id = UUID();
+CREATE TRIGGER `t1` BEFORE INSERT ON `tag` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
 END
 ;;
 delimiter ;
@@ -173,10 +298,10 @@ delimiter ;
 -- ----------------------------
 -- Triggers structure for table user
 -- ----------------------------
-DROP TRIGGER IF EXISTS `user_id_before`;
+DROP TRIGGER IF EXISTS `u1`;
 delimiter ;;
-CREATE TRIGGER `user_id_before` BEFORE INSERT ON `user` FOR EACH ROW BEGIN
-     SET new.id = UUID();
+CREATE TRIGGER `u1` BEFORE INSERT ON `user` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
 END
 ;;
 delimiter ;
