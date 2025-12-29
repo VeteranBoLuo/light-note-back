5387fe5d180e2bdc47765aaa9ecf32ac9fcbc67f
文俊邱 <1902013368@qq.com>
2025-12-29 11:20:00 +0800

    --

--- moveFile.js
+++ moveFile.js
@@ -33,7 +33,7 @@ async function migrateOldFiles() {
       // 2. 在OBS中创建唯一的对象键
       // 使用和老逻辑类似的路径，例如: files/{user_id}/{timestamp}_original_name
       // 这里加上 `migrated_` 前缀以便区分
-      const objectKey = `files/${file.create_by}/migrated_${Date.now()}_${file.file_name}`;
+      const objectKey = `files/${file.create_by}/${file.file_name}`;
 
       // 3. 上传文件到OBS
       console.log(`正在上传: ${file.file_name} -> ${objectKey}`);
@@ -41,7 +41,7 @@ async function migrateOldFiles() {
         Bucket: OBS_BUCKET_NAME,
         Key: objectKey,
         Body: fs.createReadStream(localFilePath),
-        ContentType: file.file_type
+        ContentType: file.file_type,
       });
 
       // 4. 更新数据库，设置obs_key和新的directory
@@ -54,7 +54,6 @@ async function migrateOldFiles() {
       // 5. (可选) 上传并验证成功后，删除本地文件以节省空间
       // fs.unlinkSync(localFilePath);
       // console.log(`已删除本地文件: ${localFilePath}`);
-
     } catch (error) {
       console.error(`迁移文件失败 ${file.file_name}:`, error);
       // 记录失败，但继续迁移其他文件
@@ -64,10 +63,12 @@ async function migrateOldFiles() {
 }
 
 // 执行迁移
-migrateOldFiles().then(() => {
-  console.log('所有操作完成。');
-  process.exit(0);
-}).catch(err => {
-  console.error('迁移过程发生错误:', err);
-  process.exit(1);
-});
+migrateOldFiles()
+  .then(() => {
+    console.log('所有操作完成。');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error('迁移过程发生错误:', err);
+    process.exit(1);
+  });
